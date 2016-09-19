04612a6142566d3c01e68cf6c90ef63e45266bba
liangsijian <liangsijian@foxmail.com>
2016-09-19 22:24:15 +0800

    socks connect

---
+++ common.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, with_statement
+
+import socket
+
+
+def compat_ord(s):
+    if type(s) == int:
+        return s
+    return _ord(s)
+
+
+def compat_chr(d):
+    if bytes == str:
+        return _chr(d)
+    return bytes([d])
+
+
+_ord = ord
+_chr = chr
+ord = compat_ord
+chr = compat_chr
+
+
+def to_bytes(s):
+    if bytes != str:
+        if type(s) == str:
+            return s.encode('utf-8')
+    return s
+
+
+def to_str(s):
+    if bytes != str:
+        if type(s) == bytes:
+            return s.decode('utf-8')
+    return s
+
+
+def inet_ntop(family, ipstr):
+    if family == socket.AF_INET:
+        return to_bytes(socket.inet_ntoa(ipstr))
+    elif family == socket.AF_INET6:
+        import re
+        v6addr = ':'.join(('%02X%02X' % (ord(i), ord(j))).lstrip('0')
+                          for i, j in zip(ipstr[::2], ipstr[1::2]))
+        v6addr = re.sub('::+', '::', v6addr, count=1)
+        return to_bytes(v6addr)
+
+
+def inet_pton(family, addr):
+    addr = to_str(addr)
+    if family == socket.AF_INET:
+        return socket.inet_aton(addr)
+    elif family == socket.AF_INET6:
+        if '.' in addr:  # a v4 addr
+            v4addr = addr[addr.rindex(':') + 1:]
+            v4addr = socket.inet_aton(v4addr)
+            v4addr = map(lambda x: ('%02X' % ord(x)), v4addr)
+            v4addr.insert(2, ':')
+            newaddr = addr[:addr.rindex(':') + 1] + ''.join(v4addr)
+            return inet_pton(family, newaddr)
+        dbyts = [0] * 8  # 8 groups
+        grps = addr.split(':')
+        for i, v in enumerate(grps):
+            if v:
+                dbyts[i] = int(v, 16)
+            else:
+                for j, w in enumerate(grps[::-1]):
+                    if w:
+                        dbyts[7 - j] = int(w, 16)
+                    else:
+                        break
+                break
+        return b''.join((chr(i // 256) + chr(i % 256)) for i in dbyts)
+    else:
+        raise RuntimeError("What family?")
+
+
+class BadSocksHeader(Exception):
+    pass
+
+
+class NoAcceptableMethods(Exception):
+    pass
--- proxy.py
+++ proxy.py
@@ -3,30 +3,45 @@
 
 
 import logging
+import socket
+import struct
 
 import tornado.httpserver
 import tornado.ioloop
 import tornado.iostream
 import tornado.web
 from tornado import gen, httpclient, tcpclient, httputil
+from tornado import tcpserver
 from tornado.options import options, define
+
+import common
 
 define('port', default=8888, type=int, help="listen port")
 
-__all__ = ['SSProxyHandler', 'ss_run_proxy']
+SOCKS5_VERSION = 5
+
+# SOCKS METHOD definition
+SOCKS5_METHOD_NO_AUTH = 0
+SOCKS5_METHOD_NOTHING = 0xFF
+
+# SOCKS command definition
+SOCKS5_CMD_CONNECT = 1
+SOCKS5_CMD_BIND = 2
+SOCKS5_CMD_UDP_ASSOCIATE = 3
+
+# SOCKS address type
+SOCKS5_ADDRESS_TYPE_IPV4 = 0x01
+SOCKS5_ADDRESS_TYPE_IPV6 = 0x04
+SOCKS5_ADDRESS_TYPE_HOST = 0x03
+SOCKS5_ADDRESS_TYPE_AUTH = 0x10
+SOCKS5_ADDRESS_TYPE_MASK = 0xF
 
 
-def fetch_request(url, callback, **kwargs):
-    req = tornado.httpclient.HTTPRequest(url, **kwargs)
-    client = tornado.httpclient.AsyncHTTPClient()
-    client.fetch(req, callback, raise_error=False)
-
-
-class SSProxyHandler(tornado.web.RequestHandler):
+class HttpProxyHandler(tornado.web.RequestHandler):
     SUPPORTED_METHODS = ['GET', 'POST', 'CONNECT']
 
     def __init__(self, *args, **kwargs):
-        super(SSProxyHandler, self).__init__(*args, **kwargs)
+        super(HttpProxyHandler, self).__init__(*args, **kwargs)
         self.upstream = None
         self.client = None
 
@@ -78,7 +93,6 @@ class SSProxyHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def connect(self):
         logging.info('Start CONNECT to %s from %s', self.request.uri, self.request.remote_ip)
-        # host, port = self.request.uri.split(':')
         host, port = httputil.split_host_and_port(self.request.uri)
         self.client = self.request.connection.stream
         c = tcpclient.TCPClient()
@@ -109,17 +123,152 @@ class SSProxyHandler(tornado.web.RequestHandler):
         self.client.write(b'HTTP/1.0 200 Connection established\r\n\r\n')
 
 
-def ss_run_proxy(port, start_ioloop=True):
-    app = tornado.web.Application([
-        (r'.*', SSProxyHandler),
-    ])
-    app.listen(port)
-    ioloop = tornado.ioloop.IOLoop.instance()
-    if start_ioloop:
-        ioloop.start()
+class StreamChannel(object):
+    def __init__(self, stream, address):
+        self.local_address = address
+        self.local_stream = stream
+        self.remote_address = None
+        self.remote_stream = None
+        future = self.start()
+        tornado.ioloop.IOLoop.instance().add_future(future, callback=lambda f: f.result())
+
+    def __hash__(self):
+        return id(self)
+
+    @gen.coroutine
+    def start(self):
+        r = yield self.socks5_auth()
+        if not r:
+            self.destroy()
+            raise gen.Return()
+        r = yield self.socks5_request()
+        if not r:
+            self.destroy()
+            raise gen.Return()
+        self.destroy()
+
+    @gen.coroutine
+    def socks5_auth(self):
+        # | version | nmethods | methods  |
+        # |---------+----------+----------|
+        # |       1 |        1 | 1 to 255 |
+        data = yield self.local_stream.read_bytes(257, partial=True)
+        if len(data) < 3:
+            logging.warning('method selection header too short')
+            raise gen.Return(False)
+
+        socks_version = common.ord(data[0])
+        n_methods = common.ord(data[1])
+        if socks_version != SOCKS5_VERSION:
+            logging.warning('unsupported SOCKS protocol version ' + str(socks_version))
+            raise gen.Return(False)
+        if n_methods < 1:
+            raise gen.Return(False)
+        no_auth_exist = False
+
+        methods = data[2:]
+        for method in methods:
+            if common.ord(method) == SOCKS5_METHOD_NO_AUTH:
+                no_auth_exist = True
+                break
+        if not no_auth_exist:
+            logging.warning('none of SOCKS METHOD\'s requested by client is supported')
+            raise gen.Return(False)
+        else:
+            self.local_stream.write(b'\x05\00')
+            raise gen.Return(True)
+
+    @gen.coroutine
+    def socks5_request(self):
+        # request:
+        # | VER | CMD | RSV   | ATYP | DST.ADDR | DST.PORT |
+        # |-----+-----+-------+------+----------+----------|
+        # |  1  |  1  | X'00' |   1  | Variable |    2     |
+
+        # response:
+        # | VER | REP | RSV   | ATYP | BND.ADDR | BND.PORT |
+        # |-----+-----+-------+------+----------+----------|
+        # |   1 |   1 | X'00' |    1 | Variable |        2 |
+
+        data = yield self.local_stream.read_bytes(4)
+        socks_version = common.ord(data[0])
+        if socks_version != SOCKS5_VERSION:
+            logging.warning('unsupported SOCKS protocol version ' + str(socks_version))
+            raise gen.Return(False)
+        command = common.ord(data[1])
+        if command == SOCKS5_CMD_UDP_ASSOCIATE:
+            logging.debug("SOCKS udp request is not supported")
+            # if self.local_stream.socket.family == socket.AF_INET6:
+            #     self.local_stream.write(b"\x05\x07\x00\x04\x00\xff\xff")
+            # elif self.local_stream.socket.family == socket.AF_INET:
+            #     self.local_stream.write(b"\x05\x07\x00\x04\x00\xff\xff")
+            self.local_stream.write(b"\x05\x07\x00\x01"
+                                    b"\x00\x00\x00\x00\x10\x10")
+            raise gen.Return(False)
+        elif command == SOCKS5_CMD_BIND:
+            logging.debug("SOCKS bing command is not supported")
+            self.local_stream.write(b"\x05\x07\x00\x03\x00\xff\xff")
+            raise gen.Return(False)
+        elif command != SOCKS5_CMD_CONNECT:
+            logging.debug("unsupported SOCKS request " + str(command))
+            raise gen.Return(False)
+        address_type = common.ord(data[3]) & SOCKS5_ADDRESS_TYPE_MASK
+        dest_addr = None
+        dest_port = None
+        if address_type == SOCKS5_ADDRESS_TYPE_IPV4:
+            # DST.ADDR :4 | DST.PORT | 2
+            data = yield self.local_stream.read_bytes(6)
+            dest_addr = socket.inet_ntoa(data[:4])
+            dest_port = struct.unpack("!H", data[:4])[0]
+        elif address_type == SOCKS5_ADDRESS_TYPE_IPV6:
+            # DST.ADDR :16 | DST.PORT | 2
+            data = yield self.local_stream.read_bytes(18)
+            dest_addr = common.inet_ntop(socket.AF_INET6, data[:16])
+            dest_port = struct.unpack("!H", data[16:])[0]
+        elif address_type == SOCKS5_ADDRESS_TYPE_HOST:
+            # ADDR.LEN:1 | DST.ADDR:ADDR.LEN | DST.PORT:2
+            _len = yield self.local_stream.read_bytes(1)
+            _len = common.ord(_len)
+            if _len <= 0 or _len > 128:
+                raise gen.Return(False)
+            data = yield self.local_stream.read_bytes(_len + 2)
+            dest_addr = data[:_len]
+            dest_port = struct.unpack("!H", data[_len:])[0]
+        logging.debug("SOCKS request connect to %s:%d", dest_addr, dest_port)
+        self.local_stream.write(b'\x05\x00\x00\x01'
+                                b'\x00\x00\x00\x00\x10\x10'),
+
+    def destroy(self):
+        if self.local_stream:
+            logging.debug("destroying local stream")
+            self.local_stream.close()
+
+        if self.remote_stream:
+            logging.debug("destroying remote stream")
+            self.remote_stream.close()
+
+
+class SSRunSocksServer(tcpserver.TCPServer):
+    def __init__(self, *args, **kwargs):
+        super(SSRunSocksServer, self).__init__(*args, **kwargs)
+
+    def handle_stream(self, stream, address):
+        logging.debug("connection from %s", address)
+        StreamChannel(stream, address)
+        # channel.local_stream, channel.local_address = stream, address
+
+
+def ss_run_proxy(port):
+    # app = tornado.web.Application([ (r'.*', SSProxyHandler), ])
+    # app.listen(port)
+    server = SSRunSocksServer()
+    server.listen(port)
+    server.start()
 
 
 if __name__ == '__main__':
     options.parse_command_line()
-    print ("Starting HTTP proxy on port %d" % options.port)
+    logging.info("Starting HTTP proxy on port %d" % options.port)
     ss_run_proxy(options.port)
+    loop = tornado.ioloop.IOLoop.instance()
+    loop.start()
